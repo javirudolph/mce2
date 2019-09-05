9fed17cbaef700ea91ba28f1d50a803281996f63
javirudolph <javiera.av@ufl.edu>
2019-09-05 15:30:54 -0400

    added information for the VP function at site level:

--- R/variPart_functions.R
+++ R/variPart_functions.R
@@ -37,7 +37,16 @@ get_VPresults <- function(HMSCmodel, MEMsel, numClusters,
 
 }
 
-# This function is necessary when running infor on sites.
+#' @title Variation partinioning: sites
+#' @desctiption use of the VariPart function from HMSC, gets results at the level of species
+#' @param HMSCmodel output from the metacom_as_HMSCdata() or an object from as.HMSCdata()
+#' @param MEMsel distance-based Moran' Eigenvector Map's eigenvector maps from a geographic distance matrix (xy matrix). This comes from adespatial::dbmem()
+#' @param numClusters number of clusters based on number of cores available to work with DoParallel
+#' @param makeRDS should the function make an RDS file from the output and save it? Default is FALSE
+#' @param whereToSave file path for the RDS file to be saved in
+#' @param objName should the RDS file be saved, what should it be called?
+#' @details I think this function can probably just be incorporated into the VariPart for species if we include and if statement for sites = TRUE
+#'
 get_VPresults_SITE <- function(HMSCmodel, MEMsel, numClusters,
                                makeRDS = FALSE,
                                whereToSave = NULL,
